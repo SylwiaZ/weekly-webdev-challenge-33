9db60b695e33714189f6c584e646641609c9ef73
Sylwia Zuziak <sylwia.zuz@gmail.com>
2017-05-25 14:58:48 +0200

    Finish mobile version

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -7,6 +7,11 @@ $(document).ready(function(){
         nextArrow: '<i class="icon-down"></i>',
         prevArrow: '<i class="icon-up"></i>',
         vertical: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnFocus: false,
         responsive: [
             {
                 breakpoint: 768,
@@ -23,6 +28,11 @@ $(document).ready(function(){
         infinite: true,
         nextArrow: '<i class="icon-right"></i>',
         prevArrow: '<i class="icon-left"></i>',
+        autoplay: true,
+        autoplaySpeed: 4000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        pauseOnFocus: false,
         responsive: [
             {
                 breakpoint: 768,
@@ -38,9 +48,16 @@ $(document).ready(function(){
     });
 
     var totalSlides = $('.about-us .slider .slide:not(.slick-cloned)').length;
-    console.log(totalSlides);
-    /*var currentIndex = $('div.active').index() + 1;
+    $('.about-us .total').text('0' + totalSlides);
+    $('.about-us .slider').on('afterChange', function(event, slick, currentSlide){
+        $('.about-us .current').text('0' + (currentSlide + 1));
+    });
 
-    $('.num').html(''+currentIndex+'/'+totalItems+'');*/
+    totalSlides = $('.blog .slider .slide:not(.slick-cloned)').length;
+    $('.blog .total').text('0' + totalSlides);
+    $('.blog .slider').on('afterChange', function(event, slick, currentSlide){
+        $('.blog .current').text('0' + (currentSlide + 1));
+    });
+
 
 });
