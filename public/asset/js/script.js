03cc7baa74456941ef12a86cf83d342cfd53e260
Sylwia Zuziak <sylwia.zuz@gmail.com>
2017-06-04 11:40:35 +0200

    JavaScript refactoring

--- public/asset/js/script.js
+++ public/asset/js/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
+    
+    handleNavigation();
+    handleAboutUsSlider();
+    handleBlogSlider();
 
+});
+
+function handleNavigation() {
+    /* Rozwijanie mobilnej nawigacji */
+    $('.mobile-nav .hamburger').click(function() {
+        $(this).parents('.mobile-nav').toggleClass('open');
+    });
+}
+
+function handleAboutUsSlider() {
+    /* Inicjalizacja slajdera */
     $('.about-us .slider').slick({
         dots: true,
         arrows: true,
@@ -22,6 +37,16 @@ $(document).ready(function(){
         ]
     });
 
+    /* Licznik slajdera */
+    var totalSlides = $('.about-us .slider .slide:not(.slick-cloned)').length;
+    $('.about-us .total').text('0' + totalSlides);
+    $('.about-us .slider').on('afterChange', function(event, slick, currentSlide){
+        $('.about-us .current').text('0' + (currentSlide + 1));
+    });
+}
+
+function handleBlogSlider() {
+    /* Inicjalizacja slajdera */
     $('.blog .slider').slick({
         dots: true,
         arrows: true,
@@ -43,21 +68,21 @@ $(document).ready(function(){
         ]
     });
 
-    $('.mobile-nav .hamburger').click(function() {
-        $(this).parents('.mobile-nav').toggleClass('open');
-    });
-
-    var totalSlides = $('.about-us .slider .slide:not(.slick-cloned)').length;
-    $('.about-us .total').text('0' + totalSlides);
-    $('.about-us .slider').on('afterChange', function(event, slick, currentSlide){
-        $('.about-us .current').text('0' + (currentSlide + 1));
-    });
-
-    totalSlides = $('.blog .slider .slide:not(.slick-cloned)').length;
+    /* Licznik slajdera */
+    var totalSlides = $('.blog .slider .slide:not(.slick-cloned)').length;
     $('.blog .total').text('0' + totalSlides);
     $('.blog .slider').on('afterChange', function(event, slick, currentSlide){
         $('.blog .current').text('0' + (currentSlide + 1));
     });
+}
 
 
-});
+
+
+
+
+
+
+
+
+
